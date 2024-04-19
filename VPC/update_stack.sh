1bcb069761892424102b36e8f54b6942dcf7f135
Vagrant <yelekeri@gmail.com>
2024-04-19 01:01:54 +0000

    removed vpc stack

--- VPC/update_stack.sh
+++
@@ -1,4 +0,0 @@
-aws cloudformation update-stack --stack-name vpc --template-body file://vpc.yaml $*
-
-# To change values to prod, pass the following 
-# ./update_stack.sh --parameters ParameterKey=vpcCidr,ParameterValue=/prod/vpc/vpc_cidr
