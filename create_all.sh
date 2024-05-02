d452604fa3b8194472e2524d513c52ddad32bd78
Vagrant <yelekeri@gmail.com>
2024-05-02 18:10:32 +0000

    build and deploy done

--- create_all.sh
+++ create_all.sh
@@ -3,8 +3,8 @@
 #### For signature error, run the command
 sudo /usr/sbin/ntpdate pool.ntp.org
 
-dirs=(network permissions ) 
-#dirs=(network permissions alb autoscale codebuild codedeploy) 
+#dirs=(network permissions ) 
+dirs=(network permissions alb autoscale codebuild codedeploy) 
 
 # Change into each directory and execute create stack script 
 for i in "${dirs[@]}"
